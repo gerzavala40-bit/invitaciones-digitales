9b92b87d57b70d0be21b6947f4609b663215a4c1
GER40z <gerzavala40@gmail.com>
2026-08-10 17:23:38 -0300

    Fix routing for 15anos-clara

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: https: blob:",
@@ -39,6 +39,10 @@ const nextConfig: NextConfig = {
       {
         source: '/',
         destination: '/te-invito-landing.html',
+      },
+      {
+        source: '/15anos-clara',
+        destination: '/15anos-clara.html',
       },
     ]
   },
